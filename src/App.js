45821a1d112bd515f0708ac162fd0d61c11665f9
Ovissss <ovis@DESKTOP-7VIVS9E.localdomain>
2022-02-03 17:20:14 +0000

    Nav

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 
+const page = (
+<div>
+  <h1 className="header"> This is JSX </h1>
+  <p> This is a Paragraph</p>
+</div>
+
+
+)
 
 function App() {
   return (
     <div className="App">
-     
+
+  {nav}    
+  {navbar}
       <Test></Test>
       <List /> 
       <Select options={
@@ -20,10 +30,35 @@ function App() {
       } />
       <DatePicker />
         <h1>I'm learning React !!</h1>
+  
+
     </div>
   );
 }
 
-
-
 export default App;
+
+const navbar = (
+  <nav>
+      <h1>Bob's Bistro</h1>
+      <ul>
+          <li>Menu</li>
+          <li>About</li>
+          <li>Contact</li>
+      </ul>
+  </nav>
+)
+
+
+
+const nav = (
+  <nav>
+<h1>Ovi </h1>
+<ul>
+  <li>Pricing</li>
+  <li>About</li>
+  <li>Contact</li>
+</ul>
+  </nav>
+
+)
