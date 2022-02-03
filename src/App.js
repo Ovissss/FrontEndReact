7cf6e78137be9e820e277ae73a8c258a3f37864e
Ovissss <ovis@DESKTOP-7VIVS9E.localdomain>
2022-02-03 19:07:58 +0000

    first page

--- src/App.js
+++ src/App.js
@@ -17,23 +17,25 @@ const page = (
 
 )
 
-function TemporaryName() {
+function Page() {
   return (
     <div>
-   <img src="./logo192.png"/>
-   <h1> Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originaly created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-
-    </div>
-
-  );
+      <header>
+      <nav><img src = "./logo192.png" width={150}/> </nav>
+      </header>
+        <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+          <li>It's a popular library, so I'll be 
+          able to fit in with the cool kids!</li>
+          <li>I'm more likely to get a job as a developer
+          if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2022 Graus development. All rights reserved. </small>
+        </footer>
+      </div>
+  )
 }
 
-export default TemporaryName;
+export default Page;
 
